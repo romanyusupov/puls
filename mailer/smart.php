7f76b481db492e925df4ad261f055c3fbd02ec38
Roman Yusupov <71277404+romanyusupov@users.noreply.github.com>
2022-03-13 14:45:13 +0300

    Update smart.php

--- mailer/smart.php
+++ mailer/smart.php
@@ -1,8 +1,8 @@
 <?php 
 
-$name = $_POST['name'];
-$phone = $_POST['phone'];
-$email = $_POST['email'];
+$name = $_GET['name'];
+$phone = $_GET['phone'];
+$email = $_GET['email'];
 
 require_once('phpmailer/PHPMailerAutoload.php');
 $mail = new PHPMailer;
