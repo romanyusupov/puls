33942cde314c08cabc9c5a33d009c3e97eed81ef
Roman Yusupov <71277404+romanyusupov@users.noreply.github.com>
2022-03-13 14:20:18 +0300

    Update smart.php

--- mailer/smart.php
+++ mailer/smart.php
@@ -14,7 +14,7 @@ $mail->isSMTP();                                      // Set mailer to use SMTP
 $mail->Host = 'smtp.gmail.com';  // Specify main and backup SMTP servers
 $mail->SMTPAuth = true;                               // Enable SMTP authentication
 $mail->Username = 'mindbodymailer@gmail.com';                 // Наш логин
-$mail->Password = 'Narasimha108';                           // Наш пароль от ящика
+$mail->Password = 'xpfszupruwrwvfdo';                           // Наш пароль от ящика
 $mail->SMTPSecure = 'ssl';                            // Enable TLS encryption, `ssl` also accepted
 $mail->Port = 465;                                    // TCP port to connect to
  
