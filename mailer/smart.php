ba6df984e51dcc198e8250e0950ae2a25a8fefdc
Roman Yusupov <71277404+romanyusupov@users.noreply.github.com>
2022-03-15 13:40:33 +0300

    Update smart.php

--- mailer/smart.php
+++ mailer/smart.php
@@ -14,7 +14,7 @@ $mail->isSMTP();                                      // Set mailer to use SMTP
 $mail->Host = 'smtp.yandex.ru';  // Specify main and backup SMTP servers
 $mail->SMTPAuth = true;                               // Enable SMTP authentication
 $mail->Username = 'avocod108@yandex.ru';                 // Наш логин
-$mail->Password = 'czooewnrhvvrwkmw';                       // Наш пароль от ящика
+$mail->Password = '7Ec027366';                       // Наш пароль от ящика
 $mail->SMTPSecure = 'ssl';                            // Enable TLS encryption, `ssl` also accepted
 $mail->Port = 465;                                    // TCP port to connect to
  
